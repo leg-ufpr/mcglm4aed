65541b9cb77e57d493cd9a2c05e9fb5b1edfe295
Walmes Zeviani <walmes@ufpr.br>
2017-07-22 13:30:01 -0300

    Add build.sh to deply the site.

---
+++ build.sh
@@ -0,0 +1,54 @@
+# Removes the _site/.
+rm -r _site/
+# Rscript -e "library(rmarkdown); clean_site()"
+
+# The root file.
+echo $PWD
+THEROOT=$PWD
+cd $THEROOT
+
+# List of Rmd files.
+RMDFILES=$(find . -name \*.Rmd -print)
+echo $RMDFILES
+
+# Runs rmarkdown::render() in each Rmd file down in the tree.
+for RMD in $RMDFILES; do
+    DIRNAME=`dirname "$RMD"`
+    RMDNAME=`basename "$RMD"`
+    cd $DIRNAME
+    Rscript -e "library(rmarkdown); render(\"$RMDNAME\")"
+    cd $THEROOT
+done
+
+# Renders the site.
+Rscript -e "library(rmarkdown); render_site()"
+
+# Change to the gh-pages branch.
+# git branch --delete gh-pages
+git checkout -b gh-pages
+# git checkout gh-pages
+
+# Go to the _site/.
+# cd _site/
+# tree
+
+# Force add html, js, css, png, jpg and site_libs/.
+git add -f site_libs/
+git add -f *.{html,css,png,jpg}
+
+# Undo the `git add`.
+# git reset HEAD
+
+git status
+git commit -m "Updates gh-pages"
+
+# git clean --dry-run -fdx
+# git status
+
+# Kill the gh-pages in the server.
+git push origin --delete gh-pages
+
+# Push the new gh-pages.
+git push -u origin gh-pages
+
+git checkout master
