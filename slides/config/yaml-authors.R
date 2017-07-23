8ec1fb8ca638a0d69fbb8744034da56ca17176f2
Walmes Zeviani <walmes@ufpr.br>
2017-07-22 21:10:13 -0300

    Allows authors table for html and slidy formats.

--- slides/config/yaml-authors.R
+++ slides/config/yaml-authors.R
@@ -1,9 +1,9 @@
-if (knitr::opts_knit$get("rmarkdown.pandoc.to") == "html") {
+if (knitr::opts_knit$get("rmarkdown.pandoc.to") %in% c("html", "slidy")) {
     cat("  | Wagner H. Bonat   | Walmes M. Zeviani |\n",
         "  |:-----------------:|:-----------------:|\n",
         "  | `wbonat@ufpr.br`  | `walmes@ufpr.br`  |\n",
         "  |  LEG/DEST/UFPR    |  LEG/DEST/UFPR    |\n",
         sep = "")
 } else {
-    cat("  Wagner H. Bonat & Walmes M. Zeviani")
+    cat("  Wagner H. Bonat & Walmes M. Zeviani\n")
 }
